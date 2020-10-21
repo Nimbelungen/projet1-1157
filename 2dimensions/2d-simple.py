1974f5ba8349b785a05beaf716188fb909ce28de
Quentin <45366083+quentinprieels@users.noreply.github.com>
2020-10-21 17:47:21 +0200

    Fonctions "return_hc" et "return_angle_max" qui calculent respectivement l'enfoncement de la parge dans l'eau ainsi que l'angle d'inclinaison maximum

--- 2dimensions/2d-simple.py
+++ 2dimensions/2d-simple.py
@@ -11,7 +11,7 @@ mb = 0      # masse_barge
 mg = 0      # masse_grue
 mp = 0      # masse_poid
 # mcp = 0     # masse_contre_poid
-lb = 0      # longeur_barge
+lb = 0      # longeur_barge - lorsqu un volume est necessaire, on considere la longeur = largeur
 hb = 0      # hauteur_barge
 lg = 0      # longeur_grue
 hg = 0      # hauteur_grue
@@ -19,4 +19,29 @@ dd = 0      # distance_deplacement
 theta = 0   # Angle d'inclinaison de la barge
 # --- Variables immuables ---
 g = 9.81    # Accélération gravitationelle
+
+
+# ------ Les fonctions elementaires ------
+def return_hc():
+    """Calculer la longeur immergee de la barge
+        :return: Si hc < hauteur barge : la distence hc, ou hc est la longeur suivent l'axe z immergee de la barge
+                    Sinon, False
+        """
+    sub_volume = (mg + mb + mp) / 1000
+    hc = sub_volume / (2 * lb)
+    if hc < hb:
+        return hc
+    else:
+        return False
+
+
+def return_angle_max():
+    """
+    Calcul l'angle d'inclinaison maximum de la barge. Si cette valeur est dépassée, l'au s'inflitre sur la barge
+    :return: l'angle maximum EN RADIANS (selon l axe x - cad rotation autour de l axe y, imaginaire en 2D)
+    """
+    tan_theta = (hb - return_hc()) / (lb / 2)
+    angle_max = math.atan2(tan_theta)
+    return angle_max
+
 
