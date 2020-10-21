0ce2d1e82ce6516ab21685943c596aba08a584b8
Quentin <45366083+quentinprieels@users.noreply.github.com>
2020-10-21 12:34:22 +0200

    Création de la branche "quentin" Redéfinition et réécriture des fonctions calcul_hc() et angle_max() Attention : erreurs conscientes dans les 3 autres fonctions

--- misson 4.py
+++ misson 4.py
@@ -1,15 +1,19 @@
 import math
 
+# Variables a definir
 masse_barge = 10
 masse_grue = 15
 masse_poid_deplace = 1.5
 distance_deplacement = 2
 hauteur_deplacement = 0
-g = 9.81
 longeur_barge = 1
 largeur_barge = 1
 hauteur_barge = 0.8
 hauteur_grue = 0.5
+
+# Variables fixes
+g = 9.81
+tan_theta = 0
 
 """ Initialiser les variables avec les input
 masse_barge = float(input("rentrer le masse de la barge :"))
@@ -22,7 +26,6 @@ l2 = float(input("largeur de la base : "))
 h1 = float(input("hauteur de toute la barge"))
 """
 
-
 """ATTENTION : L ENSEMBLE DES CALCULS CE FAIT DANS LE REPERE OU
 L AXE X = parrallèle à la surface de l'eau, 0 au milieu de la barge (et de la grue)
 L AXE Y = perpendiculaire à x (sur l'eau), 0 au milieu de la barge (et de la grue)
@@ -31,42 +34,45 @@ L AXE Z = verticale (perendiculaire à l'eau), 0 au niveau de l'eau
 
 
 def calcul_hc():
+    """Calculer la longeur immergee
+    :return: la distence hc, ou hc est la longeur du volume immerge
     """
-    Calculer la hauteur immergée de la barge
-    :return: La hauteure hc immergée de la barge
-    """
-    v_immerge = ((masse_barge + masse_grue) * g) / (1000 * g)
-    hc = v_immerge / (longeur_barge * largeur_barge)
+    sub_volume = (masse_grue + masse_barge + masse_poid_deplace) / 1000
+    hc = sub_volume / (longeur_barge * largeur_barge)
     return hc
 
 
-def angle_max():
+def angle_max(axe):
     """
-    :return: l'inclinaison maximum avant que la barge ne chavire à coup sur
+    Calcul l'angle d'inclinaison maximum de la barge. Si cette valeur est dépassée, l'au s'inflitre sur la barge
+    :type axe: str
+    :param axe: l'axe autaur duquel la barge va trouner : si toune autour de y => longeur ; si tourne autour de x => largeur
+    :return: l'angle maximum EN RADIANS
     """
-    thetamax = math.atan((hauteur_barge - calcul_hc()) / (longeur_barge / 2))
-    return thetamax
+    global tan_theta
+    if axe == "y":
+        tan_theta = (hauteur_barge - calcul_hc()) / longeur_barge//2
+    elif axe == "x":
+        tan_theta = (hauteur_barge - calcul_hc()) / largeur_barge // 2
+    # elif axe == "xy": todo trouver comment faire si ça tourne autour de 2 axes => si la grua à une base qui bouge
+
+    theta = math.atan2(tan_theta)
+    return theta
 
 
-def center_gravity():
-    """
-    :return: Retoune les coordonnées du centre de gravité
-    """
-    center_gravity_x = ((masse_barge * 0) + (masse_grue * 0) + (masse_poid_deplace * distance_deplacement)) / (masse_barge + masse_grue + masse_poid_deplace)
-    center_gravity_y = ((masse_barge * 0) + (masse_grue * 0) + (masse_poid_deplace * 0)) / (masse_barge + masse_grue + masse_poid_deplace)
-    center_gravity_z = ((masse_barge * (- hauteur_barge / 2)) + (masse_grue * (hauteur_barge - calcul_hc() + hauteur_grue)) + (masse_poid_deplace * hauteur_deplacement)) / (masse_barge + masse_grue + masse_poid_deplace)
-    return [center_gravity_x, center_gravity_z, center_gravity_y]
+def center_gravity(initial):
+    code
+
+
+
+def center_pousse_tra(parallel1, parallel2, hauteur, theta):
+    code
+
 
 
 def calcul_theta():
-    # 1 : Calcul du nouveau centre de gravité
+    # 1 : Calcul du nouveau centre de gravite
     # 2 : Choix de theta
-    # 3 : Calcul du nouveau
-    for i in range(1000000):
-        ca = masse_poid_deplace * g * distance_deplacement
-        cr = math.sqrt()
-        if ca == cr:
-            return
-
-
-print(calcul_hc())
+    # 3 : Calcul du nouveau centre de gravite
+    # 4 : Calcul du nouveau centre de pousse
+    code
